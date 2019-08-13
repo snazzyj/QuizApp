4916e68fd563a4e304986acd5263ecf86ee725b0
AJ Olson <silentx.alex@gmail.com>
2019-08-13 16:35:36 -0500

    Initial commit with feedback changes

---
+++ questionbank.js
@@ -0,0 +1,61 @@
+const questions = [
+  {
+    question: 'Who is the Elven sword-wielding character you play as in the Legend of Zelda Series?',
+    answers: ['Mario', 'Link', 'Steve', 'Kirby'],
+    correctAnswer: 'Link'
+  },
+
+  {
+    question: 'Who are you trying to rescue in Super Mario 64?',
+    answers: ['Goombario', 'Parakerry', 'Peach', 'Toad'],
+    correctAnswer: 'Peach'
+  },
+
+  {
+    question: 'Which of the following is NOT a ghost from the original Pac Man',
+    answers: ['Winky', 'Pinky', 'Inky', 'Clyde'],
+    correctAnswer: 'Winky'
+  },
+
+  {
+    question: 'Which of these titles were released earliest for the Nintendo 64?',
+    answers: ['Legend of Zelda Ocarina of Time','Super Smash Bros', 'Perfect Dark', 'Bomberman 64'],
+    correctAnswer:'Bomberman 64'
+  },
+
+  {
+    question: 'How many playable characters are there in Super Smash Bros?',
+    answers: ['8', '12,', '16', '10'],
+    correctAnswer: '12'
+  },
+
+  {
+    question: 'What do you collect in Banjo-Kazooie?',
+    answers: ['Musical Notes and Jinjos', 'Jiggys and Jinjos', 'Musical Notes, Jiggys, Jinjos, Empty Honeycomb Pieces', 'Musical Notes, Jiggys and Jinjos'],
+    correctAnswer: 'Musical Notes, Jiggys, Jinjos, Empty Honeycomb Pieces'
+  },
+
+  {
+    question: 'How many race tracks are there in Mario Kart 64',
+    answers: ['22', '12', '16', '20'],
+    correctAnswer: '16'
+  },
+
+  {
+    question: 'What kind of beast is Spyro?',
+    answers: ['Dragon', 'Hippogriff', 'Unicorn', 'Hydra'],
+    correctAnswer: 'Dragon'
+  },
+
+  {
+    question: "What is the name of Mario & Luigi's arch Rivals?",
+    answers: ['Bowser & Bowser Jr', 'Toad & Peach', 'Wario & Walugi', 'Donkey Kong & Diddy Kong'],
+    correctAnswer: 'Wario & Walugi'
+  },
+
+  {
+    question: 'The Pokemon Hitmonlee and Hitmonchan english translation references these 2 people?',
+    answers: ['Jackie Chan & Bruce Lee', 'Chuck Norris & Jet Li', 'Lucy Liu & Cameron Diaz', 'Liam Neeson & Jason Statham'],
+    correctAnswer: 'Jackie Chan & Bruce Lee'
+  },
+]
